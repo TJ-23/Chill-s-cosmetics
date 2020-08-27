3d6529a430ec2768bdbf12562742c14500b103c4
Azlxy <62254752+Azlxy@users.noreply.github.com>
2020-08-27 19:19:42 +0100

    Update index.js

--- thunder-bot-master/index.js
+++ thunder-bot-master/index.js
@@ -171,7 +171,7 @@ if (command === 'cosmetic') {
                 .map(
                   (o, index) =>
                     `${index}. [${o.name}](${o.icons.icon}), ${o.id}`
-                )
+                ).slice(15)
                 .join("\n"))
         .setTimestamp()
         message.channel.send(embed);
@@ -346,7 +346,7 @@ if (command === 'cosmetic') {
                 .map(
                   (o, index) =>
                     `${index}. [${o.name}](${o.icons.icon}), ${o.id}`
-                )
+                ).slice(15)
                 .join("\n"))
         .setTimestamp()
         message.channel.send(embed);
