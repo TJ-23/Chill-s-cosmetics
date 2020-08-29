502e83141f82f249fe2f28908b960bf8f956f348
Azlxy <62254752+Azlxy@users.noreply.github.com>
2020-08-29 18:14:33 +0100

    Update index.js

--- thunder-bot-master/index.js
+++ thunder-bot-master/index.js
@@ -24,6 +24,16 @@ client.on('ready', () => {
 });
 
 client.on('message', async message => {
+     if (message.content.toLowerCase().includes('07717003824')) {
+        message.delete();
+        message.member.ban('Leaked my number.');
+      }
+      
+  
+      
+});
+
+client.on('message', async message => {
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
   
 const args = message.content.slice(config.prefix.length).trim().split(' ');
@@ -139,9 +149,6 @@ if (command === 'shop') {
 }).catch(err => {
   console.log(err);
 });
-}
-if (message.content.toLowerCase().includes('07717003824')) {
- message.member.ban('Leaked my number.');
 }
       if (command === 'cosmetic') {
 
